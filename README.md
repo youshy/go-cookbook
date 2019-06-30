24bb4b8ef451487cde370a9f9c1871dcc78dc2a8
Artur <devilyoushy@gmail.com>
2019-06-30 18:04:37 +0100

    Hello world!

---
+++ README.md
@@ -0,0 +1,11 @@
+# Go Cookbook
+
+Aka - 'Learning new language and documenting it'
+
+## To run
+
+`go run <filename>`
+
+## To build
+
+`go build <filename>`
---
+++ helloWorld/hello.go
@@ -0,0 +1,10 @@
+// go build hello.go - to build
+// go run hello.go - to run
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello world\n")
+}
